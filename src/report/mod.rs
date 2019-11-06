5bd4d50c29e9414d6fa3b4fc5e76e1c2e28a5cb2
Simon Johnston <johnstonskj@gmail.com>
2019-11-06 11:44:38 -0800

    Added basic doc structure

--- src/report/mod.rs
+++ src/report/mod.rs
@@ -1,5 +1,19 @@
 /*!
 Provides the ability to produce formatted, readable, versions of a policy document.
+
+TBD
+
+# Example Usage
+
+```rust,ignore
+let generator = MarkdownGenerator::default();
+report::walk_policy(&policy, &generator, &mut stdout());
+```
+
+# Building a new Visitor
+
+TBD
+
 */
 
 // ------------------------------------------------------------------------------------------------
