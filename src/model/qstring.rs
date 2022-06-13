fdc81521cb4c9411abc3b3effdfdfdb4106cc262
Mack Stump <mack@mackstump.com>
2022-06-13 10:25:18 -0400

    fix: import Formatter and Error from fmt as opposed to private serde

--- src/model/qstring.rs
+++ src/model/qstring.rs
@@ -3,11 +3,9 @@ Provides a namespace-qualified string.
 */
 
 use regex::Regex;
-use serde::export::fmt::Error;
-use serde::export::Formatter;
 use serde::{de, de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
-use std::fmt;
 use std::fmt::Display;
+use std::fmt::{self, Error, Formatter};
 use std::str::FromStr;
 use std::string::ToString;
 
